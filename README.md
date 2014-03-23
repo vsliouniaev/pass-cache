c2282b65919af808cd5a4ed6d3804e18b377b836
vas <v.sliouniaev@gmail.com>
2014-03-23 15:49:52 +0000

    Move files around

---
+++ README.md
@@ -0,0 +1,8 @@
+Pass-Cache
+===============================
+
+A tool for securely sending a password or any other message with minimal user impact.
+
+Currently living on Azure at passcache.azurewebsites.net
+
+Suggestions and feedback welcome
---
+++ src/PassCache/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="N/A">
+//   Public domain
+// </copyright>
+// <summary>
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PassCache")]
+[assembly: AssemblyProduct("PassCache")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0407bbe1-9b05-4819-869e-75db896323c2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
